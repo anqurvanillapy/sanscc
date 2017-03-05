9deaf56332cd53851d92344a67920ea345d5ce1f
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-03-05 17:32:49 +0800

    updated lexer (generate array of tokens)

--- src/lexer.sh
+++ src/lexer.sh
@@ -3,18 +3,43 @@
 #
 #   Tokenize the flow of bytes by creating an array of specialized strings as
 #   tokens, in spite of reordering them by the syntax.
+#
+#   Note: Use printf '%s\n' "${tokens[@]}" to print the elements of tokens array
+#   separated by newlines.
+
+row=1
+col=0
+
+tokens=()
+buf=""
+
+# Error messages.
+INVALID_SYNTAX="invalid syntax"
+
+function append_token() {
+    if [[ "$buf" != "" ]]; then
+        tokens+=("$buf")
+        buf=""
+    fi
+}
 
 while IFS= read -N 1 c; do
-    token=""
+    ((col++))
     case "$c" in
-        $' ' | $'\n')
-            echo "skip!"
+        $'\n')
+            ((row++))
+            ((col=0))
+            append_token
             ;;
-        [a-z0-9])
-            echo "yes!"
+        $' ')
+            append_token
+            ;;
+        [_a-z0-9] )
+            buf="$buf$c"
             ;;
         *)
-            echo "no!"
+            echo "$row:$col "$INVALID_SYNTAX" \"$c\""
+            exit 1
             ;;
     esac
 done <&0
